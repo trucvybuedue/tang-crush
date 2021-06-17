e9d1c962327e78af6088d169cc37c154635e2189
trucvybuedue <86042647+trucvybuedue@users.noreply.github.com>
2021-06-17 14:56:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: ' Doãn à!',
+    introTitle: ' hi Doãn weeboo !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'cười qq, iu thì nhấp tiếp ',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'em là nócccc <33',
+    btnNo: 'Không nhé ,hehe :3 (thách m nhấp được ) ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'i say baby please be my friend for life 🥰. Please by my side 😘😘 ',
     messDesc: ' anni 100days quen nhau nha bé Lu , mong là sẽ thêm nhiều số 0 đằng sau nha. ',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: ' https://www.messenger.com/t/100044381719018 ' 
+    btnAccept: 'dạaaaaaaa <3',
+    messLink: ' https://www.facebook.com/messages/t/100501728201770' 
 }
